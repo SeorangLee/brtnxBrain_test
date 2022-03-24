559d07577b53bbf6ac6207dabaa8f16f90ecf4ea
AzureAD\SierraLee <sierra.lee@brtnx.com>
2022-03-24 16:40:50 +0900

    export test result xlsx

--- log.py
+++ log.py
@@ -1,20 +1,21 @@
 import json
 import csv
+import pandas as pd
+import openpyxl
 from datetime import datetime
 
 now = datetime.now()
 current_time  = now.strftime('%Y%m%d_%Hh%Mm%Ss')
 
 def mkLog (log_data) :
-    file_path="./logs/"+current_time+".json"
+    file_path="./logs/jason/"+current_time+".json"
     with open(file_path, 'w') as outfile:
         json.dump(log_data, outfile, indent=4)
         
 
 def mkLogToCSV (current_time, data):
-    file = open(current_time+".csv", "w", encoding='utf-8-sig', newline="")
-    writer = csv.DicWriter(file, fieldnames=["Test id", "Test module", "description", "Test Result"])
-    writer.writeheader()
-    writer.writerows(dicList)
-    file.close()
+    file_path="./logs/xlsx/"+current_time+".xlsx"
+    df = pd.DataFrame(data)
+    df.to_excel(file_path)
+  
 
